224efc537747b19a9dc0673e1c7d1a5d39db7f41
skroyerj <sofiejulius@gmail.com>
2024-09-23 15:27:26 +0200

    Changed name and added all floors + updated desk requirement

--- rules/Deskrule.py
+++ rules/Deskrule.py
@@ -1,16 +1,25 @@
-import bpy
-from bonsai.bim.ifc import IfcStore
-model = IfcStore.get_file()
+import ifcopenshell
+import os
 
-desks_required = 108  # Expected value of desks
+os.chdir('C:/Users/sofie/OneDrive - Danmarks Tekniske Universitet/DTU kandidat/41934 - Advanced BIM/IFC models/GR06')
+model = ifcopenshell.open('CES_BLD_24_06_ARC.ifc')
 
-# Find the building storey that corresponds to "Level 16"
+desks_required = 1620  # Expected value of desks
+
+# Find the building storey that corresponds to "Level 16" and "Level 5"
 storey_level_16 = None
+storey_level_5 = None
 for storey in model.by_type('IfcBuildingStorey'):
     if storey.Name == 'Level 16':  # Using the correct storey name
         storey_level_16 = storey
         break
 
+for storey in model.by_type('IfcBuildingStorey'):
+    if storey.Name == 'Level 5':
+        storey_level_5 = storey
+        break
+
+desks_in_storey = None
 if storey_level_16:
     # Get the desks (IfcFurniture) that are related to the found storey
     desks_in_storey = [
@@ -18,11 +27,16 @@ if storey_level_16:
         if 'desk' in furniture.Name.lower() and 
            any(rel.RelatingStructure == storey_level_16 for rel in furniture.ContainedInStructure)
     ]
+    desks_in_storey = desks_in_storey + 13*[
+        furniture for furniture in model.by_type('IfcFurniture')
+        if 'desk' in furniture.Name.lower() and 
+           any(rel.RelatingStructure == storey_level_5 for rel in furniture.ContainedInStructure)
+    ]
 
     desks_in_model = len(desks_in_storey)
-    
+
     print('\n')
-    print(f'There are {desks_in_model} desks in Level 16.')
+    print(f'There are {desks_in_model} desks in the model.')
 
     # Check to establish if we have the 'correct' number of desks
     if desks_required == desks_in_model:
@@ -32,6 +46,6 @@ if storey_level_16:
     else:
         print("There are more desks than expected")
 else:
-    print("Storey 'Level 16' not found.")
+    print("Some storey not found.")
     
 print('\n')
